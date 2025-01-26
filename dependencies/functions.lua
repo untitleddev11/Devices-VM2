0b64424e566199979e3003ce8f743295db088189
device <skibidirizzlerxd991@gmail.com>
2025-01-25 21:38:01 -0500

    Update functions.lua

--- dependencies/functions.lua
+++ dependencies/functions.lua
@@ -1,6 +1,7 @@
 local functions = {}
 
 local replicatedStorage = game:GetService("ReplicatedStorage")
+local userInputService = game:GetService("UserInputService")
 
 function functions:SearchRemotes(Name)
 	for Index, Descendant in next, replicatedStorage:GetDescendants() do
@@ -14,10 +15,30 @@ end
 
 function functions:CheckGunHold(Child)
 	if Child:IsA("Tool") and Child:FindFirstChild("Ammo") and Child:FindFirstChild("Handle") then
-		 return true
+		return true
 	end
-	
+
 	return false
+end
+
+function functions:ReturnClosestToMouse(LocalPlayer)
+	local ClosestDistance = math.huge
+	local ClosestPlayer = nil
+
+	for Index, Player in ipairs(game.Players:GetPlayers()) do
+		if Player ~= LocalPlayer and Player.Character and Player.Character:FindFirstChild("Humanoid") and Player.Character.Humanoid.Health > 0 then
+			local ScreenPos, CameraVisible = workspace.CurrentCamera:WorldToViewportPoint(Player.Character.HumanoidRootPart.Position)
+			if CameraVisible then
+				local DistanceToMouse = (Vector2.new(userInputService:GetMouseLocation().X, userInputService:GetMouseLocation().Y) - Vector2.new(ScreenPos.X, ScreenPos.Y)).Magnitude
+				if DistanceToMouse < ClosestDistance then
+					ClosestPlayer = Player
+					ClosestDistance = DistanceToMouse
+				end
+			end
+		end
+	end
+
+	return ClosestPlayer
 end
 
 return functions
