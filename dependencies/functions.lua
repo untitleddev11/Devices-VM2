fb8fa6d5847b53a2da8c45e8f12b43fb4eb9cdca
device <skibidirizzlerxd991@gmail.com>
2025-01-26 10:45:28 -0500

    Update functions.lua

--- dependencies/functions.lua
+++ dependencies/functions.lua
@@ -2,51 +2,53 @@ local functions = {}
 
 local replicatedStorage = game:GetService("ReplicatedStorage")
 local userInputService = game:GetService("UserInputService")
+local players = game:GetService("Players")
 
 function functions:SearchRemotes(Name)
-	for Index, Descendant in next, replicatedStorage:GetDescendants() do
-		if not Descendant:IsA("RemoteEvent") then continue end
-
-		if Descendant.Name == Name then
+	for _, Descendant in next, replicatedStorage:GetDescendants() do
+		if Descendant:IsA("RemoteEvent") and Descendant.Name == Name then
 			return Descendant
 		end
 	end
 end
 
 function functions:CheckGunHold(Child)
-	if Child:IsA("Tool") and Child:FindFirstChild("Ammo") and Child:FindFirstChild("Handle") then
-		return true
-	end
-
-	return false
+	return Child:IsA("Tool") and Child:FindFirstChild("Ammo") and Child:FindFirstChild("Handle")
 end
 
 function functions:ReturnClosestToMouse(LocalPlayer)
 	local ClosestDistance = math.huge
 	local ClosestPlayer = nil
 
-	for Index, Player in ipairs(game.Players:GetPlayers()) do
+	for _, Player in next, players:GetPlayers() do
+		if not Player:FindFirstChild("DataFolder") then continue end
+		
 		local PlayerCharacter = Player.Character or workspace.Players:FindFirstChild(Player.Name)
-		if Player ~= LocalPlayer and PlayerCharacter and PlayerCharacter:FindFirstChild("Humanoid") and PlayerCharacter.Humanoid.Health > 0 then
-			local ScreenPos, CameraVisible = workspace.CurrentCamera:WorldToViewportPoint(PlayerCharacter.HumanoidRootPart.Position)
-			if CameraVisible then
-				local DistanceToMouse = (Vector2.new(userInputService:GetMouseLocation().X, userInputService:GetMouseLocation().Y) - Vector2.new(ScreenPos.X, ScreenPos.Y)).Magnitude
-				if DistanceToMouse < ClosestDistance then
-					ClosestPlayer = Player
-					ClosestDistance = DistanceToMouse
+
+		if Player ~= LocalPlayer and PlayerCharacter then
+			local HumanoidRootPart = PlayerCharacter:FindFirstChild("HumanoidRootPart")
+			local Humanoid = PlayerCharacter:FindFirstChild("Humanoid")
+
+			if not HumanoidRootPart or not Humanoid then
+				HumanoidRootPart = workspace.Players[Player.Name]:FindFirstChild("HumanoidRootPart")
+				Humanoid = workspace.Players[Player.Name]:FindFirstChild("Humanoid")
+			end
+
+			if HumanoidRootPart and Humanoid and Humanoid.Health > 0 then
+				local RootToViewportPoint, IsOnScreen = workspace.CurrentCamera:WorldToViewportPoint(HumanoidRootPart.Position)
+				if IsOnScreen then
+					local MouseLocation = userInputService:GetMouseLocation()
+					local DistanceToMouse = (Vector2.new(MouseLocation.X, MouseLocation.Y) - Vector2.new(RootToViewportPoint.X, RootToViewportPoint.Y)).Magnitude
+					if DistanceToMouse < ClosestDistance then
+						ClosestPlayer = Player
+						ClosestDistance = DistanceToMouse
+					end
 				end
 			end
 		end
 	end
 
 	return ClosestPlayer
-end
-
-function functions:ReturnPlayerVelocity(Player)
-	local Old = Player.Character.HumanoidRootPart.Position
-	task.wait(0.145)
-	local Current = Player.Character.HumanoidRootPart.Position
-	return (Current - Old) / 0.145
 end
 
 return functions
